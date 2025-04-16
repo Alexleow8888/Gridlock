b19b9007e337ecce54ed31c4ca9fed1c09e19eda
Alexleow8888 <woodleoalex@gmail.com>
2025-04-16 22:17:05 +0100

    v1.07
    Fixed ray casting issues so that the enemy will move when the player is within line of sight.

--- Gridlock/Assets/Scripts/EnemyMovement.cs
+++ Gridlock/Assets/Scripts/EnemyMovement.cs
@@ -6,44 +6,66 @@ public class EnemyMovement : MonoBehaviour
 {
 
     [SerializeField] private float speed = 8f;
+    // The Enemy's speed
 
     private GameObject player;
+    // Reference for the player
 
-    private bool HasLineOfSight = false;
+    private int CanSeePlayer = 0;
+    // Stores whether the Ray is colliding with the player
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        // Finds the object that has the Player tag assigning it to the player variable
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (HasLineOfSight)
+        if (CanSeePlayer == 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            Debug.Log("Can see the player");
+            // If the enemy can see the player then move in that direction
         }
-        //Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.blue);
+        if (CanSeePlayer == 0)
+        {
+            Debug.Log("No player detected");
+        }
 
     }
 
     private void FixedUpdate()
     {
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, direction);
+        // Casts a ray towards the player
+
         if (ray.collider != null)
+            // Detects if the ray hits anything
         {
-            HasLineOfSight = ray.collider.CompareTag("Player");
-            if (HasLineOfSight)
+            if (ray.collider.CompareTag("Player"))
             {
-                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+                Debug.DrawRay(transform.position, direction * ray.distance, Color.green);
+                CanSeePlayer = 1;
+                // If the ray hits the player then sets the CanSeePlayer variable to a true state, draws a green line for debugging
             }
             else
             {
-                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+                Debug.DrawRay(transform.position, direction * ray.distance, Color.red);
+                CanSeePlayer = 0;
+                // If the ray doesn't hit the player then sets the CanSeePlayer variable to a false state, draws a red line for debugging
             }
         }
+        else
+        {
+            CanSeePlayer = 0;
+            // If the ray doesn't hit anything then sets the CanSeePlayer to a false state.
+        }
+
     }
 }
